3b46b80c724b33ce24fb07d2ccf24c8be61b551f
RitaPinto33 <100215885+RitaPinto33@users.noreply.outlook.com>
2023-06-19 21:55:44 +0100

    Socket - variaveis de instancia e construtor

--- ChineseChekers_git_ldp/src/chinesechekers_git_ldp/ClientSocket.java
+++ ChineseChekers_git_ldp/src/chinesechekers_git_ldp/ClientSocket.java
@@ -40,55 +40,6 @@ public class ClientSocket {
         this.ligado = false;
     }
 
-    /**
-     *Método que permitirá enviar uam menssagem para o servidor.
-     * @param mensagem
-     * @throws IOException
-     */
-    public void enviaMensagem(String mensagem) throws IOException {
-        if (ligado) {
-            dos.writeUTF(mensagem);
-        }
-    }
-
-    /**
-     *Método que permitirá receber e ler uma menssagem do servidor.
-     * @return
-     * @throws IOException
-     */
-    public String recebeMensagem() throws IOException {
-        String mensagem = null;
-        if (ligado) {
-            mensagem = dis.readUTF();
-        }
-        return mensagem;
-    }
-
-    /**
-     *O método liga permite fazer a ligação de um cliente com o servidor.
-     * @throws IOException
-     */
-    public void liga() throws IOException {
-        if (!ligado) {
-            InetAddress ip = InetAddress.getByName(serverName);
-            s = new Socket(ip, serverPort);
-            dis = new DataInputStream(s.getInputStream());
-            dos = new DataOutputStream(s.getOutputStream());
-        }
-        ligado = true;
-    }
-
-    /**
-     *O método desliga permite desligar o cliente do servidor.
-     * @throws IOException
-     */
-    public void desliga() throws IOException {
-        if (ligado) {
-            s.close();
-            dis = null;
-            dos = null;
-        }
-        ligado = false;
-    }
+ 
 }
 
